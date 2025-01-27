d7c4ed38dc51060b338b6a52df81917e4bbe5f24
Nianyu Shen <xiaoyu9964@gmail.com>
2025-01-27 12:28:52 -0800

    fix: add log rotation and version in logging
    Signed-off-by: Nianyu Shen <xiaoyu9964@gmail.com>

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"github.com/c3os-io/c3os/provider-rke2/pkg/provider"
 	"github.com/kairos-io/kairos-sdk/clusterplugin"
+	"github.com/kairos-io/provider-rke2/pkg/log"
+	"github.com/kairos-io/provider-rke2/pkg/provider"
 	"github.com/mudler/go-pluggable"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	log.InitLogger("/var/log/provider-rke2.log")
+
 	plugin := clusterplugin.ClusterPlugin{
 		Provider: provider.ClusterProvider,
 	}
